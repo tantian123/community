c853d04d02fc6afac98affed9e0119b9e2f5c47a
TanTian123 <1151206092@qq.com>
2020-02-04 21:27:38 +0800

    add index and bootstrap

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -2,14 +2,15 @@ package com.example.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @RequestMapping("/hello")
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);//把浏览器传过来的值放到model里面
-        return "hello";//去resource下找
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";//去resource下找
     }
 }
