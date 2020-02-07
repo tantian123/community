a0df92b1366636ba8b158696f42bae4bc3d8ad96
TanTian123 <1151206092@qq.com>
2020-02-07 22:08:07 +0800

    h2 connect

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 ## tt社区
 
 ## 资料
-https://spring.io/guides
-
-https://elasticsearch.cn/explore
-
+[Sring文档](https://spring.io/guides)  
+[示例网站](https://elasticsearch.cn/explore)  
+[github登录](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
+[bootstrap](https://www.bootcss.com/)  
+[github oauth授权登录](https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/)   
+[oauth2.0教程1](http://www.ruanyifeng.com/blog/2019/04/oauth_design.html)  
+[oauth2.0教程2](https://tools.ietf.org/html/rfc6749)  
+[okhttp](https://square.github.io/okhttp/)  
+[fastjson]()  
+[mvnrepository](https://mvnrepository.com/)  
+[mybatis](http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/index.html)  
+[mybatis-springboot](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-connect-to-production-database)  
 ## 工具
 git
 
---
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -0,0 +1,65 @@
+package com.example.community.controller;
+
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import com.example.community.mapper.UserMapper;
+import com.example.community.model.User;
+import com.example.community.provider.GithubProvider;
+import com.sun.net.httpserver.HttpsServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+
+//调用callback的时候，拿到code state  传回去
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.redirect.uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser !=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountid(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
+    }
+}
